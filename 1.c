c43a44e03e93a5da4220f43667aeb0d0e1819449
samikshaneware <140888388+samikshaneware@users.noreply.github.com>
2023-07-29 15:50:21 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,11 @@
+//Write a program to take two numbers from the user and print their sum
+#include<stdio.h>
+int main()
+{
+int a,b,s;
+scanf("%d",&a);
+scanf("%d",&b);
+s=a+b;
+printf("%d",s);
+return 0;
+}
